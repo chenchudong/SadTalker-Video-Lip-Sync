0ec9aa908e1ea0905bd9511084f9451bd5dea412
chenchudong <chenchudong@itouchtv.cn>
2023-06-01 14:58:36 +0800

    update

--- src/utils/paste_pic.py
+++ src/utils/paste_pic.py
@@ -51,6 +51,7 @@ def paste_pic(video_path, pic_path, crop_info, new_audio_path, full_video_path, 
         if enhancer_region == 'none':
             pp = ff
         else:
+            '''
             cropped_faces, restored_faces, restored_img = restorer.enhance(
                 ff, has_aligned=False, only_center_face=True, paste_back=True)
             if enhancer_region == 'lip':
@@ -67,12 +68,12 @@ def paste_pic(video_path, pic_path, crop_info, new_audio_path, full_video_path, 
             img = Laplacian_Pyramid_Blending_with_mask(restored_img, ff, full_mask[:, :, 0], 10)
             pp = np.uint8(cv2.resize(np.clip(img, 0, 255), (width, height)))
             pp, orig_faces, enhanced_faces = enhancer.process(pp, full_img_list[index], bbox=[cly, cry, clx, crx],
-                                                              face_enhance=False, possion_blending=True)
+                                                              face_enhance=False, possion_blending=True
+            
             '''
             mask = 255 * np.ones(p.shape, p.dtype)
-            location = ((cly + cry) // 2, (cly + cry) // 2)
+            location = ((clx + crx) // 2, (cly + cry) // 2)
             pp = cv2.seamlessClone(p,  full_img_list[index], mask, location, cv2.NORMAL_CLONE)
-             '''
         out_tmp.write(pp)
     out_tmp.release()
     cmd = r'ffmpeg -y -i "%s" -i "%s" -vcodec copy "%s"' % (tmp_path, new_audio_path, full_video_path)
