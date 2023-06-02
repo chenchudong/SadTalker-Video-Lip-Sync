b8a62eb810dae008b3175ea2f79c66ace9117296
chenchudong <chenchudong@itouchtv.cn>
2023-06-02 11:13:25 +0800

    update

--- src/utils/paste_pic.py
+++ src/utils/paste_pic.py
@@ -79,7 +79,7 @@ def paste_pic(video_path, pic_path, crop_info, new_audio_path, full_video_path, 
         '''
         mask = 255 * np.ones(p.shape, p.dtype)
         location = ((clx + crx) // 2, (cly + cry) // 2)
-        pp = cv2.seamlessClone(p, full_img_list[index], mask, location, cv2.NORMAL_CLONE)
+        pp = cv2.seamlessClone(p, full_img_list[full_img_list_idx], mask, location, cv2.NORMAL_CLONE)
         out_tmp.write(pp)
     out_tmp.release()
     cmd = r'ffmpeg -y -i "%s" -i "%s" -vcodec copy "%s"' % (tmp_path, new_audio_path, full_video_path)
